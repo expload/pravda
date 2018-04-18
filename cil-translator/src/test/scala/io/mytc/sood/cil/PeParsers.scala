141d467fcd304f5744c6f4a4338a12b123da097d
vovapolu <vovapolu@gmail.com>
2018-04-18 22:22:53 +0300

    Add test for PE parsing v0.1 #12

--- cil-translator/src/test/scala/io/mytc/sood/cil/PeParsers.scala
+++ cil-translator/src/test/scala/io/mytc/sood/cil/PeParsers.scala
@@ -5,11 +5,55 @@ import java.nio.file.{Files, Paths}
 
 import fastparse.byte.all._
 
+import scala.collection.mutable.ArrayBuffer
+
 class PeParsers extends FlatSpec with Matchers {
   "hello_world.exe" should "be parsed correctly" in {
-    val helloWorldExe = Files.readAllBytes(Paths.get(getClass.getResource("/hello_world.exe").getPath))
+    import PE._
+    import Tables.Info._
+    val helloWorldExe = Files.readAllBytes(Paths.get(this.getClass.getResource("/hello_world.exe").getPath))
     val ans = PE.parse(Bytes(helloWorldExe))
-    println(ans)
+
     helloWorldExe.length shouldBe 3072
+
+    ans shouldBe Right(
+      Pe(
+        PeHeader(
+          PeFileHeader(3, 224, 258),
+          PeHeaderStandardFields(1024, 1536, 0, 8958, 8192, 16384),
+          NtSpecificFields(4194304, 8192, 32768, 512),
+          PeHeaderDataDirectories(8880, 75, 24576, 12, 8192, 8, 8200, 72),
+          ArrayBuffer(
+            SectionHeader(".text", 772, 8192, 1024, 512, 1610612768),
+            SectionHeader(".rsrc", 720, 16384, 1024, 1536, 1073741888),
+            SectionHeader(".reloc", 12, 24576, 512, 2560, 1107296320)
+          )
+        ),
+        CliHeader(72, 2, 5, 8292, 576, 1, 100663298, 0, 0, 0, 0, 0, 0),
+        MetadataRoot(
+          "v4.0.30319",
+          ArrayBuffer(StreamHeader(108, 220, "#~"),
+                      StreamHeader(328, 148, "#Strings"),
+                      StreamHeader(476, 28, "#US"),
+                      StreamHeader(504, 16, "#GUID"),
+                      StreamHeader(520, 56, "#Blob"))
+        ),
+        TildeStream(
+          0,
+          38654711111L,
+          24190111578624L,
+          List(
+            ArrayBuffer(ModuleRow),
+            ArrayBuffer(TypeRefRow, TypeRefRow, TypeRefRow),
+            ArrayBuffer(TypeDefRow, TypeDefRow),
+            ArrayBuffer(MethodDefRow(8272, 0, 6278, 52, 6, 1), MethodDefRow(8280, 0, 150, 58, 10, 1)),
+            ArrayBuffer(ParamRow),
+            ArrayBuffer(MemberRefRow, MemberRefRow, MemberRefRow),
+            ArrayBuffer(CustomAttributeRow),
+            ArrayBuffer(AssemblyRow),
+            ArrayBuffer(AssemblyRefRow)
+          )
+        )
+      ))
   }
 }
