1a84c41a3ea36f76fa598524496140f524c0ebe4
vovapolu <vovapolu@gmail.com>
2018-04-26 17:21:34 +0300

    Fix arithmetic test #12

--- cil-translator/src/test/resources/arithmetics.cs
+++ cil-translator/src/test/resources/arithmetics.cs
@@ -1,10 +1,12 @@
 
 public class Program {
+   public static int x = 10;
+
    public static void Main() {
-        int a = 4 + 2;
-        int b = 4 * 2;
-        int c = 4 / 2;
-        int d = 4 % 2;
+        int a = x + 2;
+        int b = x * 2;
+        int c = x / 2;
+        int d = x % 2;
         int e = ((a + b + 42) * c + d) / 1337;
    }
 }
