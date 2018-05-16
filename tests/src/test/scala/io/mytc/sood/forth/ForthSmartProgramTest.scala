4b37726af99dcddc3085ed89d463c61934c21e5b
vovapolu <vovapolu@gmail.com>
2018-05-16 13:34:36 +0300

    Forth erc20 test mode 3, 4, 5

--- tests/src/test/scala/io/mytc/sood/forth/ForthSmartProgramTest.scala
+++ tests/src/test/scala/io/mytc/sood/forth/ForthSmartProgramTest.scala
@@ -35,16 +35,18 @@ object ForthSmartProgramTest extends TestSuite {
         |: balance $xBA concat ;
         |: allowed concat $xA1 concat  ;
         |dup1 1 == if dup2 balance sget then
-        |dup1 2 == if from balance sget dup3 <= if
+        |dup1 2 == if dup2 0 < if
+        |             from balance sget dup3 <= if
         |                  from balance dup1 sget dup4 - sput
         |                  dup3 balance dup1 sget dup4 + sput
-        |          then then
-        |dup1 3 == if dup4 balance sget dup3 <= if
+        |          then then then
+        |dup1 3 == if dup2 0 < if
+        |             dup4 balance sget dup3 <= if
         |             dup4 from allowed sget dup3 <= if
         |                  dup4 balance dup1 sget dup4 - sput
         |                  dup4 from allowed dup1 sget dup4 - sput
         |                  dup3 balance dup1 sget dup4 + sput
-        |          then then then
+        |          then then then then
         |dup1 4 == if from dup4 allowed dup3 sput then
         |dup1 5 == if dup3 dup3 allowed sget then
       """.stripMargin
@@ -53,41 +55,97 @@ object ForthSmartProgramTest extends TestSuite {
       val balance20 = bytesToAddress(0xba, 0, 0, 0, 20)
       val allowed1030 = bytesToAddress(0xa1, 0, 0, 0, 30, 0, 0, 0, 10)
 
-      runProgram[ByteString](s"10 1 $erc20", Seq(balance10 -> int32ToData(100))) ==> Right(
-        (
-          Seq(10, 1, 100).map(int32ToData),
-          Map(balance10 -> int32ToData(100))
-        ))
+      'mode1 - {
+        runProgram[ByteString](s"10 1 $erc20", Seq(balance10 -> int32ToData(100))) ==> Right(
+          (
+            Seq(10, 1, 100).map(int32ToData),
+            Map(balance10 -> int32ToData(100))
+          ))
+      }
 
-      runProgram[ByteString](s"20 5 2 $erc20",
-                             Seq(balance10 -> int32ToData(100), balance20 -> int32ToData(10)),
-                             int32ToAddress(10)) ==> Right(
-        (
-          Seq(20, 5, 2).map(int32ToData),
-          Map(balance10 -> int32ToData(95), balance20 -> int32ToData(15))
-        ))
+      'mode2 - {
+        runProgram[ByteString](s"20 5 2 $erc20",
+          Seq(balance10 -> int32ToData(100), balance20 -> int32ToData(10)),
+          int32ToAddress(10)) ==> Right(
+          (
+            Seq(20, 5, 2).map(int32ToData),
+            Map(balance10 -> int32ToData(95), balance20 -> int32ToData(15))
+          ))
 
-      runProgram[ByteString](s"10 20 5 3 $erc20",
-                             Seq(balance10 -> int32ToData(100),
-                                 balance20 -> int32ToData(10),
-                                 allowed1030 -> int32ToData(10)),
-                             int32ToAddress(30)) ==> Right(
-        (
-          Seq(10, 20, 5, 3).map(int32ToData),
-          Map(balance10 -> int32ToData(95), balance20 -> int32ToData(15), allowed1030 -> int32ToData(5))
-        ))
+        runProgram[ByteString](s"20 500 2 $erc20",
+          Seq(balance10 -> int32ToData(100), balance20 -> int32ToData(10)),
+          int32ToAddress(10)) ==> Right(
+          (
+            Seq(20, 500, 2).map(int32ToData),
+            Map(balance10 -> int32ToData(100), balance20 -> int32ToData(10))
+          ))
 
-      runProgram[ByteString](s"30 5 4 $erc20", Seq(allowed1030 -> int32ToData(10)), int32ToAddress(10)) ==> Right(
-        (
-          Seq(30, 5, 4).map(int32ToData),
-          Map(allowed1030 -> int32ToData(5))
-        ))
+        runProgram[ByteString](s"20 -10 2 $erc20",
+          Seq(balance10 -> int32ToData(100), balance20 -> int32ToData(10)),
+          int32ToAddress(10)) ==> Right(
+          (
+            Seq(20, -10, 2).map(int32ToData),
+            Map(balance10 -> int32ToData(100), balance20 -> int32ToData(10))
+          ))
+      }
 
-      runProgram[ByteString](s"10 30 5 $erc20", Seq(allowed1030 -> int32ToData(10))) ==> Right(
-        (
-          Seq(10, 30, 5, 10).map(int32ToData),
-          Map(allowed1030 -> int32ToData(10))
-        ))
+      'mode3 - {
+        runProgram[ByteString](s"10 20 5 3 $erc20",
+          Seq(balance10 -> int32ToData(100),
+            balance20 -> int32ToData(10),
+            allowed1030 -> int32ToData(10)),
+          int32ToAddress(30)) ==> Right(
+          (
+            Seq(10, 20, 5, 3).map(int32ToData),
+            Map(balance10 -> int32ToData(95), balance20 -> int32ToData(15), allowed1030 -> int32ToData(5))
+          ))
+
+        runProgram[ByteString](s"10 20 20 3 $erc20",
+          Seq(balance10 -> int32ToData(100),
+            balance20 -> int32ToData(10),
+            allowed1030 -> int32ToData(10)),
+          int32ToAddress(30)) ==> Right(
+          (
+            Seq(10, 20, 20, 3).map(int32ToData),
+            Map(balance10 -> int32ToData(100), balance20 -> int32ToData(10), allowed1030 -> int32ToData(10))
+          ))
+
+        runProgram[ByteString](s"10 20 200 3 $erc20",
+          Seq(balance10 -> int32ToData(100),
+            balance20 -> int32ToData(10),
+            allowed1030 -> int32ToData(1000)),
+          int32ToAddress(30)) ==> Right(
+          (
+            Seq(10, 20, 200, 3).map(int32ToData),
+            Map(balance10 -> int32ToData(100), balance20 -> int32ToData(10), allowed1030 -> int32ToData(1000))
+          ))
+
+        runProgram[ByteString](s"10 20 -5 3 $erc20",
+          Seq(balance10 -> int32ToData(100),
+            balance20 -> int32ToData(10),
+            allowed1030 -> int32ToData(10)),
+          int32ToAddress(30)) ==> Right(
+          (
+            Seq(10, 20, -5, 3).map(int32ToData),
+            Map(balance10 -> int32ToData(100), balance20 -> int32ToData(10), allowed1030 -> int32ToData(10))
+          ))
+      }
+
+      'mode4 - {
+        runProgram[ByteString](s"30 5 4 $erc20", Seq(allowed1030 -> int32ToData(10)), int32ToAddress(10)) ==> Right(
+          (
+            Seq(30, 5, 4).map(int32ToData),
+            Map(allowed1030 -> int32ToData(5))
+          ))
+      }
+
+      'mode5 - {
+        runProgram[ByteString](s"10 30 5 $erc20", Seq(allowed1030 -> int32ToData(10))) ==> Right(
+          (
+            Seq(10, 30, 5, 10).map(int32ToData),
+            Map(allowed1030 -> int32ToData(10))
+          ))
+      }
     }
   }
 }
