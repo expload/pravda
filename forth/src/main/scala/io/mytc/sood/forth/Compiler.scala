575d55335611a4599e51a4c0c7b1100529cd5a61
sherzod <sg@mytc.io>
2018-05-18 15:23:57 +0500

    forth: Add ByteString support

--- forth/src/main/scala/io/mytc/sood/forth/Compiler.scala
+++ forth/src/main/scala/io/mytc/sood/forth/Compiler.scala
@@ -1,8 +1,11 @@
 package io.mytc.sood.forth
 
+import com.google.protobuf.ByteString
+
+
 class Compiler {
 
-  def compile(code: String, useStdLib: Boolean = false): Either[String, Array[Byte]] = {
+  def compile(code: String, useStdLib: Boolean = true): Either[String, Array[Byte]] = {
     import io.mytc.sood.asm.Assembler
     val parser = Parser()
     val assemb = Assembler()
@@ -10,12 +13,19 @@ class Compiler {
     val prog = if (useStdLib) StdLib.defs ++ code else code
     parser.parse(prog) match {
       case Right(forthAst) ⇒ {
-        val asmAst = transl.translate(forthAst, useStdLib)
-        Right(assemb.compile(asmAst))
+        scala.util.Try {
+          val asmAst = transl.translate(forthAst, useStdLib)
+          assemb.compile(asmAst)
+        }.map(Right(_)).recover{ case err => Left(err.getMessage) }.get
       }
       case Left(err) ⇒ Left(err)
     }
   }
+
+  def compileToByteString(code: String, useStdLib: Boolean = true): Either[String, ByteString] = {
+    compile(code, useStdLib).map(ByteString.copyFrom(_))
+  }
+
 }
 
 object Compiler {
