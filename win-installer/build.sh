3300d6d3f8fda40824b3105643901ee734a7cabd
sherzod <sg@mytc.io>
2018-06-01 00:08:54 +0500

    Add windows installer creation to build (#89).
    Our infrustructure now contains docker image with wine and WixToolset
    preinstalled. Use it to create wix windows app installer.
    
    Update docs with installer info.

---
+++ win-installer/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+set -v
+
+export WINEPREFIX=/root/.wine
+
+wine /wix/heat.exe dir stage -t set-platform.xslt -gg -cg PravdaCLIComponent -ke -srd -dr PravdaCLI -sfrag -o PravdaCLI.wxs
+wine /wix/candle.exe PravdaCLI.wxs
+wine /wix/candle.exe pravda.wxs
+wine /wix/light.exe -b stage PravdaCLI.wixobj pravda.wixobj -sval -o PravdaSDK.msi
+
