ad7e632a2fdd801a19a409ddcf1111b24d233a6b
vovapolu <vovapolu@gmail.com>
2018-07-04 18:57:46 +0300

    Format sources #120

--- dotnet/src/main/scala/pravda/dotnet/translation/data/MethodTranslationCtx.scala
+++ dotnet/src/main/scala/pravda/dotnet/translation/data/MethodTranslationCtx.scala
@@ -3,8 +3,8 @@ package pravda.dotnet.translation.data
 import pravda.dotnet.parsers.Signatures
 
 final case class MethodTranslationCtx(argsCount: Int,
-                                localsCount: Int,
-                                name: String,
-                                signatures: Map[Long, Signatures.Signature],
-                                local: Boolean,
-                                void: Boolean)
+                                      localsCount: Int,
+                                      name: String,
+                                      signatures: Map[Long, Signatures.Signature],
+                                      local: Boolean,
+                                      void: Boolean)
