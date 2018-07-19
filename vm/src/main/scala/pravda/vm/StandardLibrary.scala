f63e592a26606a1763a57e9b8915af66e251f956
Aleksey Fomkin <af@mytc.io>
2018-07-19 12:35:54 +0300

    (vm) add ripemd and ed25519.verify functions to standard library #134

--- vm/src/main/scala/pravda/vm/StandardLibrary.scala
+++ vm/src/main/scala/pravda/vm/StandardLibrary.scala
@@ -1,12 +1,30 @@
 package pravda.vm
 
+import com.google.protobuf.ByteString
+import pravda.common.contrib.{ed25519, ripemd160}
+
 object StandardLibrary {
 
+  /** (pubKey: Bytes, message: Bytes, signature: Bytes) => Bool */
   final val ValidateEd25519Signature = 0x01L
 
+  /** (message: Bytes) => Bool */
+  final val CalculateRipemd160Hash = 0x02L
+
   val implementation: Map[Long, (Memory, WattCounter) => Unit] = Map(
-    ValidateEd25519Signature -> { (memory, wattcounter) =>
-      
+    ValidateEd25519Signature -> { (memory, wattCounter) =>
+      val signature = operations.bytes(memory.pop())
+      val message = operations.bytes(memory.pop())
+      val pubKey = operations.bytes(memory.pop())
+      val result = ed25519.verify(pubKey.toByteArray, message.toByteArray, signature.toByteArray)
+      wattCounter.cpuUsage((signature.size() + message.size() + pubKey.size) * WattCounter.CpuArithmetic)
+      memory.push(Data.Primitive.Bool(result))
+    },
+    CalculateRipemd160Hash -> { (memory, wattCounter) =>
+      val message = operations.bytes(memory.pop())
+      val result = ripemd160.getHash(message.toByteArray)
+      wattCounter.cpuUsage(message.size() * WattCounter.CpuArithmetic)
+      memory.push(Data.Primitive.Bytes(ByteString.copyFrom(result)))
     }
   )
 }
