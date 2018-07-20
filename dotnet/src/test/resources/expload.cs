6d5d243edcd3a947fa6879c489ae1bc1eff47b0b
vovapolu <vovapolu@gmail.com>
2018-07-20 13:24:17 +0300

    Update dotnet docs and comments in expload.cs #154

--- dotnet/src/test/resources/expload.cs
+++ dotnet/src/test/resources/expload.cs
@@ -1,9 +1,13 @@
 using System;
 
+// WARNING
+// Purpose of this dll is only to marks specific methods for translator.
+// Translator detects them and generates necessary bytecode.
 namespace Com.Expload {
+    // Special attribute to mark classes to be translated to Pravda program
     public class Program : Attribute {}
 
-    // access to the storage
+    // Access to the storage
     public class Mapping<K, V> {
        public V get(K key) { return default(V); }
        public bool exists(K key) { return false; }
@@ -12,16 +16,22 @@ namespace Com.Expload {
     }
 
     public class Info {
+        // Get address of the sender user
         public static Bytes Sender() { return null; }
     }
 
+    // Immutable array of bytes
     public class Bytes {
+       // Get the empty byte array
        public static Bytes EMPTY = null;
 
        public Bytes(params byte[] bytes) {}
 
+       // Get the i-th byte
        public byte this[int i] { get { return 0; } set { return; } }
+       // Get the sub-array
        public Bytes Slice(int start, int length) { return null; }
+       // Concatenate two Bytes
        public Bytes Concat(Bytes other) { return null; }
     }
 }
