673a82c5cd3aa0dd17cae008dc2e2ae402f19c12
vovapolu <vovapolu@gmail.com>
2018-07-20 14:47:57 +0300

    Add stdlib functions to translator #173

--- dotnet/src/test/resources/expload.cs
+++ dotnet/src/test/resources/expload.cs
@@ -34,4 +34,12 @@ namespace Com.Expload {
        // Concatenate two Bytes
        public Bytes Concat(Bytes other) { return null; }
     }
+
+    public class StdLib {
+        public static Bytes Ripemd160(Bytes bytes) { return null; }
+        public static Bytes Ripemd160(String str) { return null; }
+
+        public static bool ValidateEd25519Signature(Bytes pubKey, Bytes message, Bytes signature) { return false; }
+        public static bool ValidateEd25519Signature(Bytes pubKey, String message, Bytes signature) { return false; }
+    }
 }
