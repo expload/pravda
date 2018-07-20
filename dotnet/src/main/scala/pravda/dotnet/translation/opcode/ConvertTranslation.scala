ec564c2ad06655e349e40ea083bbf7c1bb04f6c0
vovapolu <vovapolu@gmail.com>
2018-07-20 12:46:06 +0300

    Add more convert methods #154

--- dotnet/src/main/scala/pravda/dotnet/translation/opcode/ConvertTranslation.scala
+++ dotnet/src/main/scala/pravda/dotnet/translation/opcode/ConvertTranslation.scala
@@ -10,6 +10,12 @@ object ConvertTranslation extends OneToManyTranslatorOnlyAsm {
   override def asmOpsOne(op: CIL.Op,
                          stackOffsetO: Option[Int],
                          ctx: MethodTranslationCtx): Either[TranslationError, List[Operation]] = op match {
+
+    case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToBoolean", _)) => Right(cast(Data.Type.Boolean))
+    case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToChar", _)) => Right(cast(Data.Type.Int16))
+    case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToDouble", _)) => Right(cast(Data.Type.Number))
+    case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToInt16", _)) => Right(cast(Data.Type.Int16))
+    case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToInt32", _)) => Right(cast(Data.Type.Int32))
     case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToString", _)) => Right(cast(Data.Type.Utf8))
     case Call(MemberRefData(TypeRefData(_, "Convert", "System"), "ToByte", _))   => Right(cast(Data.Type.Int8))
     case _                                                                       => Left(UnknownOpcode)
