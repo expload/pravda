8da17d8cdea64e809ba5b7b9a8b67e3f5aa8eb32
vovapolu <vovapolu@gmail.com>
2018-08-23 16:09:44 +0300

    Uncomment TranslationVisualizer, update docs #153

--- dotnet/src/main/scala/pravda/dotnet/translation/TranslationVisualizer.scala
+++ dotnet/src/main/scala/pravda/dotnet/translation/TranslationVisualizer.scala
@@ -17,67 +17,58 @@
 
 package pravda.dotnet.translation
 
-//import pravda.dotnet.translation.data._
-//import pravda.dotnet.translation.opcode.OpcodeTranslator
-//import pravda.vm.asm.PravdaAssembler
-//import pravda.vm.asm
+import pravda.dotnet.translation.data._
+import pravda.dotnet.translation.opcode.OpcodeTranslator
+import pravda.vm.asm.PravdaAssembler
+import pravda.vm.asm
 
 object TranslationVisualizer {
-//  private def renderAsmOp(op: asm.Operation): String =
-//    PravdaAssembler.render(op, pretty = true).replace('\n', ' ')
-//
-//  private def visualizeOpcode(opcode: OpCodeTranslation): List[(String, String)] = {
-//    val sourceColumnRaw = opcode.source.fold(List(_), _.map(_.toString))
-//
-//    val sourceColumnHead = (sourceColumnRaw match {
-//      case head :: _ => s"[<$head> "
-//      case _         => "["
-//    }) + s"stack_offset=${opcode.stackOffset.fold("none")(_.toString)}]"
-//
-//    val sourceColumnTail = sourceColumnRaw match {
-//      case _ :: tail => tail.map(s => s"[<$s>]")
-//      case _         => List.empty
-//    }
-//
-//    val sourceColumn = sourceColumnHead :: sourceColumnTail
-//
-//    opcode.asmOps
-//      .map(renderAsmOp)
-//      .zipAll(sourceColumn, "", "")
-//  }
-//
-//  private def visualizeMethod(method: MethodTranslation): String = {
-//    val opcodesColumns = method.opcodes.flatMap(visualizeOpcode)
-//    val firstColumn = opcodesColumns.map(_._1).map(_.length).max
-//    val opcodes = opcodesColumns
-//      .map {
-//        case (fst, snd) => fst.padTo(firstColumn + 1, ' ') + snd
-//      }
-//      .mkString("\n")
-//
-//    s"""|[method ${method.name} args=${method.argsCount} locals=${method.localsCount} func=${method.func}]
-//        |$opcodes""".stripMargin
-//  }
-//
-//  private def visualizeFunction(func: OpcodeTranslator.HelperFunction): String = {
-//    s"""|[function ${func.name}]
-//        |${func.ops.map(renderAsmOp).mkString("\n")}""".stripMargin
-//  }
-//
-//  def visualize(translation: Translation): String = {
-//    s"""|[jump to methods]
-//        |${translation.jumpToMethods.map(renderAsmOp).mkString("\n")}
-//        |
-//        |[methods]
-//        |${translation.methods.map(visualizeMethod).mkString("\n")}
-//        |
-//        |[funcs]
-//        |${translation.funcs.map(visualizeMethod).mkString("\n")}
-//        |
-//        |[functions]
-//        |${translation.helperFunctions.map(visualizeFunction).mkString("\n")}
-//        |
-//        |[finish]
-//        |${translation.finishOps.map(renderAsmOp).mkString("\n")}""".stripMargin
-//  }
+  // TODO simplify or rewrite, meta infromation
+
+  private def renderAsmOp(op: asm.Operation): String =
+    PravdaAssembler.render(op, pretty = true).replace('\n', ' ')
+
+  private def visualizeOpcode(opcode: OpCodeTranslation): List[(String, String)] = {
+    val sourceColumnRaw = opcode.source.fold(List(_), _.map(_.toString))
+    val sourceColumn = sourceColumnRaw.map(c => s"[<$c>]")
+
+    opcode.asmOps
+      .map(renderAsmOp)
+      .zipAll(sourceColumn, "", "")
+  }
+
+  private def visualizeMethod(method: MethodTranslation): String = {
+    val opcodesColumns = method.opcodes.flatMap(visualizeOpcode)
+    val firstColumn = opcodesColumns.map(_._1).map(_.length).max
+    val opcodes = opcodesColumns
+      .map {
+        case (fst, snd) => fst.padTo(firstColumn + 1, ' ') + snd
+      }
+      .mkString("\n")
+
+    s"""|[method]
+        |$opcodes""".stripMargin
+  }
+
+  private def visualizeFunction(func: OpcodeTranslator.HelperFunction): String = {
+    s"""|[function ${func.name}]
+        |${func.ops.map(renderAsmOp).mkString("\n")}""".stripMargin
+  }
+
+  def visualize(translation: Translation): String = {
+    s"""|[jump to methods]
+        |${translation.jumpToMethods.map(renderAsmOp).mkString("\n")}
+        |
+        |[methods]
+        |${translation.methods.map(visualizeMethod).mkString("\n")}
+        |
+        |[funcs]
+        |${translation.funcs.map(visualizeMethod).mkString("\n")}
+        |
+        |[functions]
+        |${translation.helperFunctions.map(visualizeFunction).mkString("\n")}
+        |
+        |[finish]
+        |${translation.finishOps.map(renderAsmOp).mkString("\n")}""".stripMargin
+  }
 }
