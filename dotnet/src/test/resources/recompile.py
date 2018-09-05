93234c119a112f20ce0f6ed4098cb49157b2cf91
vovapolu <vovapolu@gmail.com>
2018-09-05 16:26:31 +0300

    Add new recompile script and recompile all the files #205

--- dotnet/src/test/resources/recompile.py
+++ dotnet/src/test/resources/recompile.py
@@ -20,49 +20,57 @@ def run_if_changed(name, inputs, script, outputs):
         print(f"Executing {name}...")
         for input in inputs:
             shutil.copy2(input, tmp_dir)
+        print(script(inputs, outputs))
         subprocess.call(script(inputs, outputs))
         for output in outputs:
-            shutil.copy2(output, ".")
+            shutil.copy2(in_tmp_dir(output), ".")
 
+def compile(inputs, outputs, flags):
+    res = ["csc", in_tmp_dir(inputs[0]),
+           f"-out:{in_tmp_dir(outputs[0])}"]
+
+    if len(inputs) > 1:
+        for ref in inputs[1:]:
+            res += [f"-reference:{in_tmp_dir(ref)}"]
+
+    return res + flags
 
 def compile_dll(inputs, outputs):
-    return ["csc", in_tmp_dir(inputs(0)),
-            "-target:library",
-            f"-out:{in_tmp_dir(outputs(0))}"]
+    return compile(inputs, outputs, ["-target:library"])
 
 def compile_exe(inputs, outputs):
-    return ["csc", in_tmp_dir(inputs(0)),
-            f"-reference:{in_tmp_dir(inputs(1))}",
-            f"-out:{in_tmp_dir(outputs(0))}"]
+    return compile(inputs, outputs, [])
 
 def compile_exe_pdb(inputs, outputs):
-    compile_exe(inputs, outputs[:1]) + ["-debug:portable", f"-pdb:{in_tmp_dir(outputs(1))}"]
+    return compile_exe(inputs, outputs[:1]) + \
+           ["-debug:portable", f"-pdb:{in_tmp_dir(outputs[1])}"]
 
-run_if_changed("expload.dll compilation", ["expload.cs"], compile_dll, ["expload.dll"])
+run_if_changed("expload.dll compilation",
+               ["expload.cs"], compile_dll, ["expload.dll"])
 
-for filename in ["arithmetics", "array", "closure", "compare", "error", "hello_world",
-                 "if", "inheritance", "loop", "loop_nested", "method_calling", "objects",
-                 "stdlib", "strings", "system", "zoo_program"]:
-    run_if_changed(f"{filename} compilation", [f"{filename}.cs", "expload.dll"], compile_exe, [f"{filename}.exe"])
+for filename in ["arithmetics",
+                 "arrays",
+                 "closure",
+                 "compare",
+                 "error",
+                 "if",
+                 "inheritance",
+                 "loop",
+                 "loop_nested",
+                 "method_calling",
+                 "objects",
+                 "stdlib",
+                 "strings",
+                 "system",
+                 "zoo_program"]:
+    run_if_changed(f"{filename} compilation",
+                   [f"{filename}.cs", "expload.dll"], compile_exe, [f"{filename}.exe"])
 
-for filename in ["smart_program"]:
-    run_if_changed(f"{filename} compilation", [f"{filename}.cs", "expload.dll"], compile_exe_pdb, [f"{filename}.exe", f"{filename}.pdb"])
+for filename in ["smart_program", "hello_world"]:
+    run_if_changed(f"{filename} compilation",
+                   [f"{filename}.cs", "expload.dll"], compile_exe_pdb, [f"{filename}.exe", f"{filename}.pdb"])
 
-
-for file in glob.glob("*.cs"):
-    filename = file[:-3]
-    if file in exclude:
-        continue
-
-    print("Compiling " + file + "...")
-
-    shutil.copy2(file, tmp_dir)
-
-    args = ["csc", in_tmp_dir(file),
-            "-reference:" + in_tmp_dir("expload.dll"),
-            "-out:" + in_tmp_dir(filename + ".exe")]
-    if file in with_pdb:
-        args.append("-debug:portable")
-    subprocess.call(args)
-
-    shutil.copy2(in_tmp_dir(filename + ".exe"), ".")
+run_if_changed("pcall_program compilation",
+               ["pcall_program.cs", "expload.dll"], compile_dll, ["pcall_program.dll"])
+run_if_changed("pcall compilation",
+               ["pcall.cs", "expload.dll", "pcall_program.dll"], compile_exe, ["pcall.exe"])
