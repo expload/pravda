3249ed5c2596676b67b6a2a5811c6400671a59f8
Evgeny <dedok1486@gmail.com>
2018-09-27 20:39:11 +0500

    Add tests #261
    Signed-off-by: Evgeny <dedok1486@gmail.com>

--- vm/src/main/scala/pravda/vm/standard/ExponentialFunction.scala
+++ vm/src/main/scala/pravda/vm/standard/ExponentialFunction.scala
@@ -38,14 +38,46 @@ object ExponentialFunction extends FunctionDefinition {
   override def apply(memory: Memory, wattCounter: WattCounter): Unit = {
     implicit class DoubleExp(x: Double) {
       def **(y: Double): Double = math.pow(x, y)
+      def **(y: Byte): Double = math.pow(x, y.toDouble)
+      def **(y: Int): Double = math.pow(x, y.toDouble)
+      def **(y: Long): Double = math.pow(x, y.toDouble)
+      def **(y: Short): Double = math.pow(x, y.toDouble)
+    }
+    implicit class LongExp(x: Long) {
+      def **(y: Double): Long = math.pow(x.toDouble, y).toLong
+      def **(y: Byte): Long = math.pow(x.toDouble, y.toDouble).toLong
+      def **(y: Int): Long = math.pow(x.toDouble, y.toDouble).toLong
+      def **(y: Long): Long = math.pow(x.toDouble, y.toDouble).toLong
+      def **(y: Short): Long = math.pow(x.toDouble, y.toDouble).toLong
+    }
+    implicit class IntExp(x: Int) {
+      def **(y: Double): Int = math.pow(x.toDouble, y).toInt
+      def **(y: Byte): Int = math.pow(x.toDouble, y.toDouble).toInt
+      def **(y: Int): Int = math.pow(x.toDouble, y.toDouble).toInt
+      def **(y: Long): Int = math.pow(x.toDouble, y.toDouble).toInt
+      def **(y: Short): Int = math.pow(x.toDouble, y.toDouble).toInt
+    }
+    implicit class ShortExp(x: Short) {
+      def **(y: Double): Short = math.pow(x.toDouble, y).toShort
+      def **(y: Byte): Short = math.pow(x.toDouble, y.toDouble).toShort
+      def **(y: Int): Short = math.pow(x.toDouble, y.toDouble).toShort
+      def **(y: Long): Short = math.pow(x.toDouble, y.toDouble).toShort
+      def **(y: Short): Short = math.pow(x.toDouble, y.toDouble).toShort
+    }
+    implicit class ByteExp(x: Byte) {
+      def **(y: Double): Int = math.pow(x.toDouble, y).toInt
+      def **(y: Byte): Int = math.pow(x.toDouble, y.toDouble).toInt
+      def **(y: Int): Int = math.pow(x.toDouble, y.toDouble).toInt
+      def **(y: Long): Int = math.pow(x.toDouble, y.toDouble).toInt
+      def **(y: Short): Int = math.pow(x.toDouble, y.toDouble).toInt
     }
 
     def getCost(x: Double, y: Double): Long = {
-      (math.log10(math.abs(x) + 1) / div * math.abs(y) / 8).round
+      (math.log10(math.abs(x) + 1) / div * math.abs(y) / 8).toLong + 1
     }
 
     def getBigIntCost(x: scala.BigInt, y: Int): Long = {
-      (x.toString().length / div * math.abs(y) / 8).round
+      (x.abs.toString().length / div * math.abs(y) / 8).toLong + 1
     }
     wattCounter.cpuUsage(CpuArithmetic)
     val a = memory.pop()
@@ -58,189 +90,189 @@ object ExponentialFunction extends FunctionDefinition {
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
 
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.doubleValue()))
-            Int32((lhs.toDouble ** rhs.doubleValue()).toInt)
+            Int32(lhs ** rhs.doubleValue())
           case Number(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case _ => throw VmErrorException(WrongType)
         }
       case Int16(lhs) =>
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16((lhs ** rhs.toDouble).toShort)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16((lhs ** rhs.toDouble).toShort)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs.toDouble).toShort)
+            Int16((lhs ** rhs.toDouble).toShort)
           case Number(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int16((lhs.toDouble ** rhs).toShort)
+            Int16((lhs ** rhs).toShort)
           case _ => throw VmErrorException(WrongType)
         }
       case Int8(lhs) =>
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Int32((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            BigInt((lhs.toDouble ** rhs.toDouble).toInt)
+            Int32(lhs ** rhs)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            BigInt((lhs.toDouble ** rhs.doubleValue()).toInt)
-          case Number(rhs) => Number(lhs.toDouble ** rhs)
+            Int32(lhs ** rhs.doubleValue())
+          case Number(rhs) => Number((lhs ** rhs).toDouble)
           case _           => throw VmErrorException(WrongType)
         }
       case Uint8(lhs) =>
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint8(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint8(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint8(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint8(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint8(lhs ** rhs)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint8(lhs ** rhs)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs.doubleValue()).toInt)
+            Uint8(lhs ** rhs.doubleValue())
           case Number(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint8((lhs.toDouble ** rhs).toInt)
+            Uint8(lhs ** rhs)
           case _ => throw VmErrorException(WrongType)
         }
       case Uint16(lhs) =>
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint16(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint16(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint16(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint16(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint16(lhs ** rhs)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.toDouble).toInt)
+            Uint16(lhs ** rhs)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs.doubleValue()).toInt)
+            Uint16(lhs ** rhs.doubleValue())
           case Number(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint16((lhs.toDouble ** rhs).toInt)
+            Uint16(lhs ** rhs)
           case _ => throw VmErrorException(WrongType)
         }
       case Uint32(lhs) =>
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.toDouble).toLong)
+            Uint32(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.toDouble).toLong)
+            Uint32(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.toDouble).toLong)
+            Uint32(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.toDouble).toLong)
+            Uint32(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.toDouble).toLong)
+            Uint32(lhs ** rhs)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.toDouble).toLong)
+            Uint32(lhs ** rhs)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs.doubleValue()).toLong)
+            Uint32(lhs ** rhs.doubleValue())
           case Number(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Uint32((lhs.toDouble ** rhs).toLong)
+            Uint32(lhs ** rhs)
           case _ => throw VmErrorException(WrongType)
         }
       case Number(lhs) =>
         b match {
           case Int8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Number(lhs ** rhs.toDouble)
+            Number(lhs ** rhs)
           case Int16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Number(lhs ** rhs.toDouble)
+            Number(lhs ** rhs)
           case Int32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Number(lhs ** rhs.toDouble)
+            Number(lhs ** rhs)
           case Uint8(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Number(lhs ** rhs.toDouble)
+            Number(lhs ** rhs)
           case Uint16(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Number(lhs ** rhs.toDouble)
+            Number(lhs ** rhs)
           case Uint32(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
-            Number(lhs ** rhs.toDouble)
+            Number(lhs ** rhs)
           case BigInt(rhs) =>
             wattCounter.memoryUsage(getCost(lhs.toDouble, rhs.toDouble))
             Number(lhs ** rhs.doubleValue())
@@ -252,28 +284,28 @@ object ExponentialFunction extends FunctionDefinition {
       case BigInt(lhs) =>
         b match {
           case Int8(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue()))
             BigInt(lhs.pow(rhs.toInt))
           case Int16(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue))
             BigInt(lhs.pow(rhs.toInt))
           case Int32(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue))
             BigInt(lhs.pow(rhs))
           case Uint8(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue))
             BigInt(lhs.pow(rhs))
           case Uint16(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue))
             BigInt(lhs.pow(rhs))
           case Uint32(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue))
             BigInt(lhs.pow(rhs.toInt))
           case BigInt(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toDouble.intValue()))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue()))
             BigInt(lhs.pow(rhs.intValue()))
           case Number(rhs) =>
-            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.toInt))
+            wattCounter.memoryUsage(getBigIntCost(lhs, rhs.intValue))
             BigInt(lhs.pow(rhs.toInt))
           case _ => throw VmErrorException(WrongType)
         }
