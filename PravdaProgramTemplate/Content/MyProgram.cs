35d865fc8a5b2a7f0ce62c6bc02576be273716e7
Aleksey Fomkin <af@mytc.io>
2018-11-17 18:19:11 +0300

    add pravda program template #360

---
+++ PravdaProgramTemplate/Content/MyProgram.cs
@@ -0,0 +1,13 @@
+namespace NAMESPACE {
+
+    using Expload.Pravda;
+    using System;
+
+    [Program]
+    public class MyProgram {
+
+        public string HelloWorld() {
+            return "Hello, world!";
+        }
+    }
+}
